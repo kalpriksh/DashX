e24816ed1e9b128ff079f860c36011067c99f16a
Pallavi Verma <pallavi.verma@cygrp.com>
2020-12-17 15:07:06 +0530

    Added header and sidenav components.

---
+++ DashX-Dashboard/src/app/pages/dashboard/shared/shared.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+
+import { HeaderComponent } from './header/header.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { DashboardModule } from '../dashboard.module';
+
+@NgModule({
+  declarations: [SidenavComponent,HeaderComponent],
+  imports: [
+    CommonModule,
+    MatListModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    DashboardModule
+  ],
+  exports: [
+    SidenavComponent,
+    HeaderComponent  
+  ]
+})
+export class SharedModule { }
