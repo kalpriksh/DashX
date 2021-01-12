5cf748e77ed830cd67c7967f2bb102ea4bea74e9
CYBERGINDIA\pallavi.verma <pallavi.verma@cygrp.com>
2021-01-13 02:39:54 +0530

    Added list items component to 'X-axis' and 'Series'

--- DashX-Dashboard/src/app/pages/dashboard/dashboard.module.ts
+++ DashX-Dashboard/src/app/pages/dashboard/dashboard.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms' 
-
+import {MatMenuModule} from '@angular/material/menu';
 import { MatCardModule } from "@angular/material/card";
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from "@angular/material/input";
