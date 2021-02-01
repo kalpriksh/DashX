cc0a33d7d5a143bd4b6bc52cece56ac71b9fefd7
kalpriksh <kalpriksh.bist@cygrp.com>
2021-02-01 11:10:05 +0530

    updated chart setup component for series and categories

--- DashX-Dashboard/src/app/pages/dashboard/components/chart-setup/chart-setup.component.ts
+++ DashX-Dashboard/src/app/pages/dashboard/components/chart-setup/chart-setup.component.ts
@@ -1,5 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { ChartEditorService } from '../../services'
+import { ChartSetup } from '../../component-classes';
+import { SeriesData } from '../../models';
+import { BarChart } from '../../component-classes';
 
 @Component({
   selector: 'app-chart-setup',
@@ -10,23 +13,101 @@ export class ChartSetupComponent implements OnInit {
   
   panelOpenState = true;
 
-  // bind SetupData to the UI
+  // binds SetupData to the UI
   SetupData
 
-  constructor(private data : ChartEditorService){}
+  //#region classes
+  ChartSetup : ChartSetup;
+  BarChart : BarChart;
+  //#endregion
+
+  //#region UI variables
+  seriesNames : string[];
+  seriesList : SeriesData[];
+  categoryList : any[]
+  //#endregion
+
+  constructor(private chartData : ChartEditorService){}
 
   ngOnInit(){
-    this.data.editorData_current.subscribe(chartData => this.SetupData = chartData)
-    console.log(this.SetupData);
-    
+    this.chartData.editorData_current.subscribe(chartData => this.SetupData = chartData)
+    this.seriesNames = ["null"]
+    this.seriesList = []
+    this.categoryList = []
+    this.BarChart = new BarChart();
   }
 
   EnterSubmit(event, form){
-
-    if (event.keyCode === 13) {
-      this.data.EditorDataUpdated(this.SetupData)
+    if (event.keyCode == 13) {
+      this.chartData.EditorDataUpdated(this.SetupData)
     }
-    
+  }
+
+  OnSheetUpload(workBookData){
+    /**
+     * init ChartSetup class
+     * takes entire workbook processes it and returns required data
+     */
+    this.ChartSetup = new ChartSetup(workBookData);
+    this.UpdateChartSetup();
+  }
+
+  // updates the component UI
+  UpdateChartSetup(){
+    this.seriesNames = this.ChartSetup.GetSeriesName();
+  }
+
+  AddSeries(name : string){
+    /**
+     * adds series to the ui    
+     * adds series to the chartData object
+     */
+    // default case
+    if(name == "null"){
+      alert("need to add data file")
+    }
+    else{
+      const series  = this.BarChart.CreateNewSeries(name, this.ChartSetup.GetSeriesData(name));
+
+      // update seriesList UI
+      this.seriesList.push(series)
+
+      // update setup data
+      this.SetupData.series.push(series);
+      
+      // update chart
+      this.chartData.EditorDataUpdated(this.SetupData)
+    }
+  }
+
+  
+  AddCategory(name : string){
+    /**
+     * adds series to the ui    
+     * adds series to the chartData object
+     */
+    // default case
+    if(name == "null"){
+      alert("need to add data file")
+    }
+    else{
+      const categoryData = this.ChartSetup.GetSeriesData(name);
+
+      // update seriesList UI
+      this.categoryList = categoryData;
+
+      // update setup data
+      this.SetupData.xaxis.categories = categoryData;
+      
+      // update chart
+      this.chartData.EditorDataUpdated(this.SetupData)
+    }
+  }
+
+  SaveChartData(){
+    /**
+     * saves chartData to DB
+     */
   }
 
 }
