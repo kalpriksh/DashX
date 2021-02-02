afa1564e503fa747ca8b24d9cce054215d3896e2
CYBERGINDIA\pallavi.verma <pallavi.verma@cygrp.com>
2021-02-03 04:26:47 +0530

    Made changes to update pie chart using chart editor.

--- DashX-Dashboard/src/app/pages/dashboard/component-classes/pieChart.ts
+++ DashX-Dashboard/src/app/pages/dashboard/component-classes/pieChart.ts
@@ -1,14 +1,61 @@
 import { BaseChart } from '../base'
-
+import {
+    ApexNonAxisChartSeries,
+    ApexResponsive,
+    ApexChart
+  } from "ng-apexcharts";
+import { SeriesData } from '../models';
+  
+  export type PieChartOptions = {
+    series: ApexNonAxisChartSeries;
+    chart: ApexChart;
+    responsive: ApexResponsive[];
+    labels: any;
+  };
 // class for defining the behavior and rules for the given chart type
 export class PieChart extends BaseChart{
+    seriesData : SeriesData
+
+    pieChartDefaultData : Partial<PieChartOptions>
     constructor(){
-        super()
+      super()
+    this.pieChartDefaultData = {
+          
+      series: [],
+      chart: {
+        width: 380,
+        type: "pie"
+      },
+      labels: [],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: "bottom"
+            }
+          }
+        }
+      ]
+    };
+    
+    this.seriesData = {
+        name : "",
+        data : []
+      }
     }
     GetDefaults(){
-
+      return this.pieChartDefaultData;
     }
     DataComplete(){
-
+      
     }
-} 
+    CreateNewSeries(name : string , data){
+      this.seriesData.name = name;
+      this.seriesData.data = data;
+      return this.seriesData;
+    }
+  }
--- DashX-Dashboard/src/app/pages/dashboard/components/pie-chart/pie-chart.component.ts
+++ DashX-Dashboard/src/app/pages/dashboard/components/pie-chart/pie-chart.component.ts
@@ -1,53 +1,60 @@
-import { PieChartData } from '../../../models';
-import { Component, OnInit, Input } from "@angular/core";
+import { PieChartData } from '../../models';
+import { Component, OnInit, Input, ViewChild } from "@angular/core";
 import { ChartComponent } from "ng-apexcharts";
-
-import {
-  ApexNonAxisChartSeries,
-  ApexResponsive,
-  ApexChart
-} from "ng-apexcharts";
-
-export type ChartOptions = {
-  series: ApexNonAxisChartSeries;
-  chart: ApexChart;
-  responsive: ApexResponsive[];
-  labels: any;
-};
+import { ChartEditorService } from '../../services'
+import { PieChart, PieChartOptions } from '../../component-classes/pieChart';
 
 @Component({
-  selector: 'app-pnie-chart',
+  selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
   styleUrls: ['./pie-chart.component.css']
 })
 export class PieChartComponent implements OnInit {
   public chart: Partial<any>;
-  @Input() pieChartData: PieChartData;
-  ngOnInit(): void {
-    this.chart = this.initChart(this.pieChartData.series, this.pieChartData.labels);
+  @Input() pieChartData: PieChartData;  
+  pieChartDefaults : PieChart;
+  isEditorOpen : boolean
+  EditorData
+  @ViewChild ('chartObj') chartObj : ChartComponent;
+
+  constructor(private data : ChartEditorService){
+
   }
-  public initChart(series: number[], labels: string[]): Partial<any> {
-    return {
-      series: series,
-      chart: {
-        width: 380,
-        type: "pie"
-      },
-      labels: labels,
-      responsive: [
-        {
-          breakpoint: 480,
-          options: {
-            chart: {
-              width: 200
-            },
-            legend: {
-              position: "bottom"
-            }
-          }
-        }
-      ]
-    };
+  
+
+  ngOnInit(): void {
+
+    this.pieChartDefaults = new PieChart();
+    this.chart = this.initChart()
+    this.data.isEditorOpen_current.subscribe( isOpen => this.isEditorOpen = isOpen )
+    this.data.editorData_current.subscribe( graphData => {
+      
+      this.EditorData = graphData
+      if(this.chartObj != null){
+        this.updateChart(graphData)
+      }
+
+    })
+
+  }
+
+  //#region component functions
+
+  //to update the chart options
+  updateChart( chartOption : Partial<PieChartOptions> ){
+    this.chartObj.updateOptions(
+      chartOption
+    );
+  }
+  
+  // to open chart editor
+  EditChart()
+  {
+    this.data.ToggleEditor(!this.isEditorOpen);
+  }
+
+  public initChart(): Partial<any> {
+   return  this.pieChartDefaults.GetDefaults()
   }
 }
 
