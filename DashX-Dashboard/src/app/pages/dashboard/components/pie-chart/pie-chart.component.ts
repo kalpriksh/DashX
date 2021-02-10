b7b0c9fb59458df814c24575e2c881baa01feb80
kalpriksh <kalpriksh.bist@cygrp.com>
2021-02-10 10:45:33 +0530

    included title to pie charts | conditions added for chart modification

--- DashX-Dashboard/src/app/pages/dashboard/components/pie-chart/pie-chart.component.ts
+++ DashX-Dashboard/src/app/pages/dashboard/components/pie-chart/pie-chart.component.ts
@@ -10,30 +10,37 @@ import { PieChart, PieChartOptions } from '../../component-classes/pieChart';
   styleUrls: ['./pie-chart.component.css']
 })
 export class PieChartComponent implements OnInit {
+  
   public chart: Partial<PieChartOptions>;
   @Input() pieChartData: PieChartData;  
   pieChartDefaults : PieChart;
   isEditorOpen : boolean
+  editorState : [boolean, number]
+
   EditorData
   @ViewChild ('chartObj') chartObj : ChartComponent;
-
   constructor(private data : ChartEditorService){
-
   }
   
 
   ngOnInit(): void {
 
-    this.pieChartDefaults = new PieChart();
+    this.pieChartDefaults = new PieChart(102);
     this.chart = this.initChart()
-    this.data.isEditorOpen_current.subscribe( isOpen => this.isEditorOpen = isOpen )
+    
+    // on toggle edit chart
+    this.data.isEditorOpen_current.subscribe( _editorState =>{ 
+      this.isEditorOpen = _editorState[0]
+      this.editorState = _editorState
+    })
+
+    // on data modified event
     this.data.editorData_current.subscribe( graphData => {
-      
       this.EditorData = graphData
-      if(this.chartObj != null){
+      if(this.chartObj != null){        
+        if(graphData.chartType == "Pie")
         this.updateChart(graphData)
       }
-
     })
 
   }
@@ -41,7 +48,7 @@ export class PieChartComponent implements OnInit {
   //#region component functions
 
   //to update the chart options
-  updateChart( chartOption : Partial<PieChartOptions> ){
+  updateChart( chartOption : Partial<PieChartOptions>){
     this.chartObj.updateOptions(
       chartOption
     );
@@ -49,11 +56,27 @@ export class PieChartComponent implements OnInit {
   
   // to open chart editor
   EditChart(){
-    this.data.ToggleEditor(!this.isEditorOpen);
+    debugger
+    if(this.isEditorOpen){
+      //if editor is already open
+      if(this.editorState[1] == this.pieChartDefaults.chartId){
+        // chart data is already loaded on the editor
+        this.data.ToggleEditor(!this.editorState[0], this.pieChartDefaults.chartId);
+      } 
+      else{
+        // need to load chart data on editor
+        this.data.ToggleEditor(this.editorState[0], this.pieChartDefaults.chartId)
+        this.data.EditorDataUpdated(this.pieChartDefaults)
+      }
+    }
+    else{
+        this.data.ToggleEditor(!this.editorState[0], this.pieChartDefaults.chartId)
+        this.data.EditorDataUpdated(this.pieChartDefaults)
+    }
   }
 
   public initChart(): Partial<any> {
-   return  this.pieChartDefaults.GetDefaults()
+    return  this.pieChartDefaults.GetDefaults()
   }
 }
 
