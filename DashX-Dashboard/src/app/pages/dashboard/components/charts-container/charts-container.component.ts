17fe9d1e661a7c60c3f5e2c1c9bd35c786e2fbf8
kalpriksh <kalpriksh.bist@cygrp.com>
2021-03-09 12:15:35 +0530

    button to add chart | menu to select chartType

--- DashX-Dashboard/src/app/pages/dashboard/components/charts-container/charts-container.component.ts
+++ DashX-Dashboard/src/app/pages/dashboard/components/charts-container/charts-container.component.ts
@@ -1,6 +1,7 @@
 import { DashboardService } from "../../services";
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild, ComponentFactoryResolver, ViewContainerRef } from '@angular/core';
 import { LineChartData, PieChartData, BarGraphData } from '../../models';
+import { Dashboard } from '../../component-classes'
 
 @Component({
   selector: 'app-charts-container',
@@ -8,19 +9,85 @@ import { LineChartData, PieChartData, BarGraphData } from '../../models';
   styleUrls: ['./charts-container.component.css'],
   encapsulation: ViewEncapsulation.None
 })
+
 export class ChartsContainerComponent implements OnInit {
-  
+  /**
+   * pass dashboard object to this class
+   * dashboard object should contain : child chart components
+   * chart components needs to loaded dynamically
+   */
+  @ViewChild('test', { static: true, read: ViewContainerRef }) entry: ViewContainerRef;
+
   public lineChartData: LineChartData;
   public pieChartData: PieChartData;
   public barGraphData: BarGraphData;
 
-  constructor(private service: DashboardService) {
+  showChartTypesList = false
+
+
+  // list of chart types {placeholder}
+  listOfChartTypes = ["Kpi", "Bar", "Pie"]
+  // dashboard object to edit/save
+  _dashboard : Dashboard
+  // contains the list chart in a dashboard
+  listChartObjects
+  listChartPosition
+
+  constructor(private service: DashboardService, private _resolver : ComponentFactoryResolver) {
     this.lineChartData = this.service.loadLineChartData();
     this.pieChartData = this.service.loadPieChartData();
     this.barGraphData = this.service.loadBarGraphData();
-   }
+  }
 
   ngOnInit(): void {
+    this.listChartObjects = ["kpi", "kpi", "kpi", "kpi","bar","add"];
+    this.listChartPosition = [[1,1],[1,1],[1,1],[1,1],[3,2],[1,1]];
+    
+    this._dashboard = new Dashboard(this.listChartObjects, this.listChartPosition)
+    this.DashboardInit(this._dashboard);
+  }
+
+  DashboardInit(dashboardObject){
+    /**
+     * initializes the dashboard
+     */
+    console.log(dashboardObject);
+    
+  }
+
+  AddChart(chartType){
+    /**
+     * add chart type on selection basis
+     */
+
+    console.log(chartType);
+    
+  }
+  
+
+  DeleteChart(chartId){
+    /**
+     * will be called from chartSetup
+     * remove from UI
+     * update dashboard
+     */
+
+  }
+
+  UpdateDashboard(){
+    /**
+     * updates the dashboard | add/deletes chart
+     * updates the list of charts in the dashboard
+     */
+
+  }
+
+  SaveDashboard(){
+    /**
+     * creates dashboard object
+     * returns the object to be saved
+     */
+
   }
 
 }
