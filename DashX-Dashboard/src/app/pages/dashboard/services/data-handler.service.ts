2a21c5f3531a0746c60a470846a53a0749b70345
kalpriksh <kalpriksh.bist@cygrp.com>
2021-05-25 12:44:52 +0530

    DAS-23 #comment added Metric field to dummy data

--- DashX-Dashboard/src/app/pages/dashboard/services/data-handler.service.ts
+++ DashX-Dashboard/src/app/pages/dashboard/services/data-handler.service.ts
@@ -15,61 +15,71 @@ export class DataHandlerService {
         "Country": "Colombia",
         "Id": 1,
         "CreatedOn": "12/13/2011",
-        "Population": 50415
+        "Population": 50415,
+        "Metric" : 60000
       },
       {
         "Country": "Rwanda",
         "Id": 2,
         "CreatedOn": "01/18/2011",
-        "Population": 19070
+        "Population": 19070,
+        "Metric" : 60000
       },
       {
         "Country": "Uganda",
         "Id": 3,
         "CreatedOn": "12/27/2003",
-        "Population": 25892
+        "Population": 25892,
+        "Metric" : 60000
       },
       {
         "Country": "Senegal",
         "Id": 4,
         "CreatedOn": "05/23/2022",
-        "Population": 52150
+        "Population": 52150,
+        "Metric" : 60000
       },
       {
         "Country": "Benin",
         "Id": 5,
         "CreatedOn": "06/19/2018",
-        "Population": 86003
+        "Population": 86003,
+        "Metric" : 60000
       },
       {
         "Country": "Korea, North",
         "Id": 6,
         "CreatedOn": "05/21/2021",
-        "Population": 95054
+        "Population": 95054,
+        "Metric" : 60000
       },
       {
         "Country": "Virgin Islands, British",
         "Id": 7,
         "CreatedOn": "04/24/2018",
-        "Population": 36511
+        "Population": 36511,
+        "Metric" : 60000
       },
       {
         "Country": "Seychelles",
         "Id": 8,
         "CreatedOn": "06/21/2007",
-        "Population": 39741
+        "Population": 39741,
+        "Metric" : 60000
       },
       {
         "Country": "Lebanon",
         "Id": 9,
         "CreatedOn": "07/18/2012",
-        "Population": 75397
+        "Population": 75397,
+        "Metric" : 60000
       },
       {
         "Country": "Swaziland",
         "Id": 10,
         "CreatedOn": "10/24/2021",
-        "Population": 74968
+        "Population": 74968,
+        "Metric" : 60000
       }
     ]`
   }
